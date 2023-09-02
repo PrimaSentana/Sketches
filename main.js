06ef477abcb3d58242e21931df04b426f2f5dc16
PrimaSentana <premasentana123@gmail.com>
2023-09-02 14:28:20 +0800

    adding eventListener to grid item

--- main.js
+++ main.js
@@ -3,7 +3,10 @@ const container = document.querySelector('.container');
 function makeGrid(rows, cols) {
     for (let i = 0; i < (rows * cols); i++) {
         const cells = document.createElement('div');
-        container.appendChild(cells).className = 'grid-item';
+        container.appendChild(cells).className = 'grid-item'; 
+        cells.addEventListener('click', (cell) => {
+            cell.target.style.backgroundColor = '#354f52';
+        });
     };
 };
 
